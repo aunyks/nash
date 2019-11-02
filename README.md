3473203f871e2a5b4ef82007c3386b53eb0c2dd9
Gerald Nash <me@aunyks.com>
2019-11-02 10:16:51 -0400

    Close #1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nash
+Inspired by [this tweet](https://twitter.com/aunyks/status/1071068941869559809). Progress can be tracked [here](https://github.com/aunyks/nash/projects).
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::io::{self, Write};
+use std::env;
+
+fn main() {
+    // Init shell prompt.
+    // If PS1 doesnt exist in the env
+    // we can just use the default prompt
+    let shell_prompt = match env::var("PS1") {
+        Ok(val) => val,
+        Err(_) => {
+            String::from("nash > ")
+        },
+    };
+    
+    // https://doc.rust-lang.org/std/io/struct.Stdin.html#examples
+    // The above could be useful for reading from stdin as in from a pipe later on
+
+    loop {
+        // Present to the user
+        print!("{}", shell_prompt);
+        match io::stdout().flush() {
+            Ok(()) => {},
+            Err(e) => {
+                eprintln!("{}", e);
+                std::process::exit(1)
+            }
+        }
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                // TODO: When adding evaluation,
+                //       this needs to be much
+                //       more generalized
+                if input == "exit\n" {
+                    std::process::exit(0)
+                }
+                print!("{}", input)
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+                std::process::exit(1)
+            },
+        }
+    }
+}
